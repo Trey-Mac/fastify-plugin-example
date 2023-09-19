7323c683abfe8b3fc7c1287983489f9d06090bba
Ralph Greaves <rgreaves@ycp.edu>
2023-09-19 18:04:04 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,10 +1,7 @@
 export default async function plugin(fastify, options) {
   fastify.get('/', async function (request, reply) {
-    return `
-<html>
-<body>
-  <img src="https://cataas.com/cat"></img>
-</body>
-</html>`;
+    reply
+      .header('Content-Type', 'text/html')
+      .send(`<html><body><img src="https://cataas.com/cat"></img></body></html>`);
   });
 }
